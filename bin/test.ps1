91d40966ca4725958a152954432a5f0ba54de8de
beyondmeat <51850644+beyondmeat@users.noreply.github.com>
2022-07-10 16:17:03 -0700

    Fix cemu, citra, ppsspp, rpcs3, xemu and more (#654)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- bin/test.ps1
+++ bin/test.ps1
@@ -1,4 +1,5 @@
-#requires -Modules @{ ModuleName = 'Pester'; ModuleVersion = '4.4.0' }
+#Requires -Modules @{ ModuleName = 'Pester'; MaximumVersion = '4.99' }
 
-if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
-Invoke-Pester "$psscriptroot/.."
+if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
+$result = Invoke-Pester "$PSScriptRoot\.." -PassThru
+exit $result.FailedCount
