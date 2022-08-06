4dd85793137c27cc55e5f14c0b6b9ad7524a5bfa
Ryan Caezar Itang <sitiom@proton.me>
2022-08-06 20:15:53 +0800

    Add migration script

---
+++ scripts/polymc/migrate.ps1
@@ -0,0 +1,28 @@
+$files = @(
+    "accounts",
+    "assets",
+    "instances",
+    "libraries",
+    "meta",
+    "themes",
+    "translations",
+    "accounts.json",
+    "polymc.cfg",
+    "notifications.json"
+)
+
+$options =
+    (New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", "Continue"),
+    (New-Object System.Management.Automation.Host.ChoiceDescription "&No", "Exit")
+$decision = $Host.UI.PromptForChoice("Migrate PolyMC config to Scoop",
+    "This will overwrite your current configuration. Do you want to continue?",
+    $options, 0)
+
+if ($decision -eq 0) {
+    foreach ($file in $files) {
+        $source = "$env:APPDATA\PolyMC\$file"
+        if (Test-Path $source) {
+            Copy-Item -Path $source -Destination $PSScriptRoot\$file -Recurse -Force
+        }
+    }
+}
